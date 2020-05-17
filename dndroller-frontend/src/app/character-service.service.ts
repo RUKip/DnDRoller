3b9d8c8f51af49512182b24bc8bbbff962236676
r kip <gjr.kip@gmail.com>
2020-05-17 16:46:22 +0200

    update service

--- dndroller-frontend/src/app/character-service.service.ts
+++ dndroller-frontend/src/app/character-service.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Settings } from './settings';
 import { Character } from './character';
@@ -7,11 +7,9 @@ import { Character } from './character';
   providedIn: 'root'
 })
 export class CharacterService {
-
   settings;
   http;
-  characters: Array<Character>;
-
+  
   constructor(http: HttpClient) {
     this.http = http;
     this.settings = new Settings();
@@ -19,15 +17,11 @@ export class CharacterService {
 
   loadCharacters(party_name: string) {
     const url: string = 'http://' + this.settings.defaultUrl + '/api/characters/' + party_name;
-    this.characters = this.http.get(url);
+    return  this.http.get(url);
   }
 
   saveCharacters(characters: Array<Character>) {
     const url: string = 'http://' + this.settings.defaultUrl + '/api/characters/store';
     return this.http.post(url, characters);
-  }
-
-  store(character: Character) {
-      this.characters.push(character);
   }
 }
--- dndroller-frontend/src/app/heroes/heroes.component.ts
+++ dndroller-frontend/src/app/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { CharacterService } from '../character-service.service';
+import { Character } from '../character';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +11,31 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  characterForm;
 
-  hero_list = []
-  enemy_list = []
+  constructor(
+    private characterService: CharacterService,
+    private formBuilder: FormBuilder
+    ) {
+    this.characterService = characterService;
+    this.characterForm = this.formBuilder.group({
+      name: '',
+      max_hp: 0
+    });
+   }
+
+  hero_list: Array<Character>;
+  enemy_list: Array<Character>;
 
   ngOnInit(): void {
+    
+  }
+  
+  onSubmit() {
+    let name = this.characterForm.get('name').value
+    let max_hp = this.characterForm.get('max_hp').value
+    this.hero_list.push(new Character(0, name, max_hp, max_hp))
+    this.characterForm.reset();
   }
 
 }
